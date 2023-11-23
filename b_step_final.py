d3aec92d65d58da363d8012026ecfbfb392c1aa2
sthr630-arcturus <s_izumi630y@yahoo.co.jp>
2023-11-23 22:40:12 +0900

    edit b_step3 b_step_final

--- b_step_final.py
+++ b_step_final.py
@@ -1,30 +1,23 @@
 import math
-
 n = int(input())
-max_num = 100000
-
-digit = [100000]*(max_num+1)
-digit[0] = 0
-digit[1] = 0
-
+max_num = 100
+#digit_list = [0]*int(math.sqrt(max_num)+1)
+digit_list = [0]*(max_num+1)
 for i in range(0, n):
     m = int(input())
     for j in range(2, int(math.sqrt(max_num)+1)):
+    #for j in range(2, max_num+1):
         counter = 0
         while m%j == 0:
             counter += 1
             m = m//j
-        digit[j] = min(digit[j], counter)
-
-    for j in range(int(math.sqrt(max_num)+1), max_num+1):
-        digit[j] = 0
-
+        digit_list[j] = max(digit_list[j], counter)
     if m != 1:
-        digit[m] = 1
+        digit_list[m] = 1
 
 ans = 1
 
-for i, v in enumerate(digit):
+for i, v in enumerate(digit_list):
     ans = ans * i**v
 
 print(ans)
